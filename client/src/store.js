f24882aece3acafa3ff6629cc974a585109173a9
Liam <terroridk@gmail.com>
2020-06-07 00:25:49 -0400

    fix read prop apply of undefined error

--- client/src/store.js
+++ client/src/store.js
@@ -1,17 +1,14 @@
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 const initialState = [];
 const middleware = [thunk];
 const store = createStore(
     rootReducer,
     initialState,
-    compose(
-        applyMiddleware(...middleware),
-        window.__REDUX_DEVTOOLS_EXTENSION__ &&
-            window.__REDUX_DEVTOOLS_EXTENSION__()
-    )
+    composeWithDevTools(applyMiddleware(...middleware))
 );
 
 export default store;
